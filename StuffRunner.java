5b1efbe3c4f3bb4e606375eb4a48257b3287a315
VincentH0404 <146112123+VincentH0404@users.noreply.github.com>
2023-12-14 17:06:23 +0000

    commit

---
+++ StuffRunner.java
@@ -0,0 +1,9 @@
+public class StuffRunner {
+    public static void main(String[] args) {
+
+        Stuff stuff = new Stuff(10);
+
+        stuff.printAddAmount(6);
+        System.out.println(stuff.getValue());
+    }
+}
